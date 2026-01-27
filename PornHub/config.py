5b0bb33f9fbb5563bf186324b8b8da7113f896c1
solobro44000-blip <solobro44000@gmail.com>
2026-01-27 18:29:01 +0530

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -1,13 +1,10 @@
 from typing import List
 
-API_ID: int = "insert_your_own_api_id_here"
-API_HASH: str = "insert_your_own_api_hash_here"
-TOKEN: str = "insert_your_bot_token_here"
+API_ID: int = "37176482"
+API_HASH: str = "74a311a7b29c25951f1158fbd9ad176c"
+TOKEN: str = "8570642473:AAFYjPhyEP_1i8Wi5XKtPkg50ksbtHyJRxA"
 
-log_chat: int = -1001662591986
-sub_chat: str = "SakuraEmpireTeam"
-sudoers: List[int] = [2137482758, 1249591948]
-prefixs: List[str] = ["/", "!", ".", "$", "-"]
+log_chat: int = -5256755543
 
 # notes
 
