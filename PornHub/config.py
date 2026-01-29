6e076c6b2fe6cacbcb2ff06e8cff083ef5e88b9c
solobro44000-blip <solobro44000@gmail.com>
2026-01-29 06:57:57 +0530

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -1,14 +1,9 @@
+import os
 from typing import List
 
-API_ID: int = "37176482"
-API_HASH: str = "74a311a7b29c25951f1158fbd9ad176c"
-TOKEN: str = "8570642473:AAFYjPhyEP_1i8Wi5XKtPkg50ksbtHyJRxA"
+# Read from Environment Variables (set these in Render later)
+API_ID: int = int(os.environ.get("API_ID", "0"))
+API_HASH: str = os.environ.get("API_HASH", "")
+TOKEN: str = os.environ.get("TOKEN", "")
 
-log_chat: int = -5256755543
-
-# notes
-
-# 1. api_id & api_hash get from my.telegram.org
-# 2. token fill with your bot_token get from @BotFather
-# 3. log_chat fill with the chat id of a group that you should create for the bot logger
-# 4. sub_chat fill with the channel username but don't include the '@' symbol
+# ... rest of your code (log_chat, sub_chat, etc.) ...
