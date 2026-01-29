159cd3c47d4a341df99acce7b97cf9ac0bf2f874
solobro44000-blip <solobro44000@gmail.com>
2026-01-29 07:30:03 +0530

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -5,5 +5,5 @@ from typing import List
 API_ID: int = int(os.environ.get("API_ID", "0"))
 API_HASH: str = os.environ.get("API_HASH", "")
 TOKEN: str = os.environ.get("TOKEN", "")
-
+log_chat: int = -5256755543
 # ... rest of your code (log_chat, sub_chat, etc.) ...
